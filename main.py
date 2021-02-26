349115e866947341dabd4c9e3675bf635b867115
Valeria0620 <valeriakornyleva96102@gmail.com>
2021-02-26 13:20:41 +0300

    First code

---
+++ main.py
@@ -0,0 +1,16 @@
+number1 = int(input("Введите произвольное число"))
+numberpogr = int(input("Введите пограничное число"))
+if number1/numberpogr > 3:
+    print("Произвольное число больше пограничного болеее, чем в 3 раза")
+elif number1 > numberpogr:
+    print("Произвольное число больше пограничного")
+elif number1 < numberpogr:
+    print("Произвольное число меньше пограничного")
+
+
+
+
+
+
+
+
